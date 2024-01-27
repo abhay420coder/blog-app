210dd1546e509c1e5d17d62785e3dec0cb63deb7
abhay420coder <ak8294836065@gmail.com>
2024-01-28 02:08:24 +0530

    - react initialize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.scss';
-import Homepage from "./pages/Homepage"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p className="text-red-400 font-extrabold">Hi </p>
-      <p className="rd">Hi 2 </p>
-      {/* <button className="middle none center rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">Button</button> */}
-      <Homepage />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
