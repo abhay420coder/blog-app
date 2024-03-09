1369a2eacba73b7e94eccc41fe77ab92caa6b768
abhay420coder <ak8294836065@gmail.com>
2024-03-09 23:06:47 +0530

    -navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+// import library
+import { Routes , Route} from "react-router-dom";
+
+// import component
+import { Navbar} from "./components/navbar.component";  // for new version
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
+        <>
+            {/* using shared component */}
+            <Navbar />
+            
+
+            {/* route shared component */}
+            <Routes>
+                <Route path="/" element={<h1>home Page</h1>} />
+                {/* <Route path="/" element={<HomePage />} /> */}
+                <Route path="/editor" element={<h1>Editor Page</h1>} />
+                {/* <Route path="/editor" element={<EditorPage />} /> */}
+                <Route path="/signIn" element={<h1>Sign In Page</h1>} />
+                {/* <Route path="/signIn" element={<SignInPage />} /> */}
+                <Route path="/signUp" element={<h1>Sign Up Page</h1>} />
+                {/* <Route path="/signUp" element={<SignUpPage />} /> */}
+                <Route path="*" element={<h1>Error 404 Page</h1>} />
+                {/* <Route path="*" element={<ErrorPage />} /> */}
+            </Routes>
+
+        </>
+        
+
     )
 }
 
